9131f407349f3f3a6b5d5af63b1cb9483d428a51
jesse <hitchhikes4mail@gmail.com>
2018-12-28 14:54:26 -0800

    add dataFields and updated UiFunctionality

--- examples/multi-user example/UiFunctionality.py
+++ examples/multi-user example/UiFunctionality.py
@@ -1,11 +1,13 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 from UiCore import Ui_MainDialog
-
+from dataFields import user,menuItem,tableOrder
 class UiFunctional(Ui_MainDialog):
     def __init__(self):
         self.MainDialog = QtWidgets.QDialog()
         self.setupUi(self.MainDialog)
-        
+        self.m_orders = []
+        self.m_users = []
+        self.createUsers()
         #connect push buttons
         self.toGoOrder_btn.clicked.connect(lambda:self.stackedWidget.setCurrentIndex(2))
         self.appetizers_btn.clicked.connect(lambda:self.stackedWidget_2.setCurrentIndex(0))
@@ -13,9 +15,18 @@ class UiFunctional(Ui_MainDialog):
         self.drinks_btn.clicked.connect(lambda:self.stackedWidget_2.setCurrentIndex(2))
         self.desserts_btn.clicked.connect(lambda:self.stackedWidget_2.setCurrentIndex(3))
         self.finished_btn.clicked.connect(lambda:self.stackedWidget.setCurrentIndex(0))
-
+        self.newTable_btn.clicked.connect(lambda:self.stackedWidget.setCurrentIndex(1))
+        self.one_btn.clicked.connect(lambda:self.lineEdit.insert("1"))
+        
+        
+    def createUsers(self):
+        mainUser = user("john",123456)
+        self.m_users.append(mainUser)
+    
+        
 if __name__ == "__main__":
     import sys
+    
     app = QtWidgets.QApplication(sys.argv)
     ui = UiFunctional()
     ui.MainDialog.show()
