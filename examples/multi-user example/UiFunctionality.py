d60dc7db69132901f6d8bd36ec41852a94232560
irontrack <jesseharper42@gmail.com>
2018-12-29 10:07:20 -0800

    connected login push buttons

--- examples/multi-user example/UiFunctionality.py
+++ examples/multi-user example/UiFunctionality.py
@@ -8,21 +8,38 @@ class UiFunctional(Ui_MainDialog):
         self.m_orders = []
         self.m_users = []
         self.createUsers()
-        #connect push buttons
+        
+        #connect user select push buttons
         self.toGoOrder_btn.clicked.connect(lambda:self.stackedWidget.setCurrentIndex(2))
+        self.newTable_btn.clicked.connect(lambda:self.stackedWidget.setCurrentIndex(1))
+        
+        #connect login push buttons
+        self.one_btn.clicked.connect(lambda:self.lineEdit.insert("1"))
+        self.two_btn.clicked.connect(lambda:self.lineEdit.insert("2"))
+        self.three_btn.clicked.connect(lambda:self.lineEdit.insert("3"))
+        self.four_btn.clicked.connect(lambda:self.lineEdit.insert("4"))
+        self.five_btn.clicked.connect(lambda:self.lineEdit.insert("5"))
+        self.six_btn.clicked.connect(lambda:self.lineEdit.insert("6"))
+        self.seven_btn.clicked.connect(lambda:self.lineEdit.insert("7"))
+        self.eight_btn.clicked.connect(lambda:self.lineEdit.insert("8"))
+        self.nine_btn.clicked.connect(lambda:self.lineEdit.insert("9"))
+        self.zero_btn.clicked.connect(lambda:self.lineEdit.insert("0"))
+        self.Cancel_btn.clicked.connect(self.cancel_pushed)
+        self.clear_btn.clicked.connect(self.lineEdit.clear)
+        
+        #connect order push buttons
         self.appetizers_btn.clicked.connect(lambda:self.stackedWidget_2.setCurrentIndex(0))
         self.entrees_btn.clicked.connect(lambda:self.stackedWidget_2.setCurrentIndex(1))
         self.drinks_btn.clicked.connect(lambda:self.stackedWidget_2.setCurrentIndex(2))
         self.desserts_btn.clicked.connect(lambda:self.stackedWidget_2.setCurrentIndex(3))
         self.finished_btn.clicked.connect(lambda:self.stackedWidget.setCurrentIndex(0))
-        self.newTable_btn.clicked.connect(lambda:self.stackedWidget.setCurrentIndex(1))
-        self.one_btn.clicked.connect(lambda:self.lineEdit.insert("1"))
-        
         
     def createUsers(self):
-        mainUser = user("john",123456)
+        mainUser = user("john","123456")
         self.m_users.append(mainUser)
-    
+    def cancel_pushed(self):
+        self.lineEdit.clear()
+        self.stackedWidget.setCurrentIndex(0)
         
 if __name__ == "__main__":
     import sys
