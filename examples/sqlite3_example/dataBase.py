cc06e70794b835043b29d6ea5576dc8f4b73c2dd
irontrack <jesseharper42@gmail.com>
2019-01-17 11:53:07 -0800

    turned dataBase into class

--- examples/sqlite3_example/dataBase.py
+++ examples/sqlite3_example/dataBase.py
@@ -6,61 +6,84 @@ will be created and a class for handling these will be created and stored in thi
 '''
 
 import sqlite3
+from dataFields import user, menuItem, tableOrder
 
-#Create connection
-conn = sqlite3.connect(":memory:")
-
-# Create a cursor
-c = conn.cursor()
-
-# Create users Table
-c.execute('''CREATE TABLE users (
-    user_id integer PRIMARY KEY, 
-    password text NOT NULL,
-    first_name text NOT NULL,
-    last_name text NOT NULL )
-
-    ''')
-# commit this action
-conn.commit()
-
-
-c.execute('''CREATE TABLE IF NOT EXISTS orders (
-    order_id integer PRIMARY KEY,
-    user_id integer NOT NULL,
-    table_number integer,
-    sales_subtotal real,
-    time_opened text,
-    time_closed text,
-    split integer,
-    guest_count,
-    FOREIGN KEY(user_id) REFERENCES users(user_id))
-    ''')
-
-# commit this action
-conn.commit()
-
-# creates a sales table to keep track of individual items solds and the orders they
-# belong to.
-c.execute('''CREATE TABLE IF NOT EXISTS sales(
-    sales_id integer PRIMARY KEY,
-    item_id integer,
-    order_id,
-    FOREIGN KEY(item_id) REFERENCES items(item_id),
-    FOREIGN KEY(order_id) REFERENCES orders(order_id))
-    ''')
-conn.commit()
-
-# creates an items table for each menu item
-c.execute('''CREATE TABLE IF NOT EXISTS items(
-    item_id integer PRIMARY KEY,
-    price real NOT NULL,
-    name text NOT NULL)
-    ''')
-conn.commit()
-
-
-conn.close()
+class posDatabase():
+    def __init__(self):
+        #Create connection
+        self.conn = sqlite3.connect(":memory:")
+        
+        # Create a cursor
+        self.c = self.conn.cursor()
+        
+        # Create users Table
+        self.c.execute('''CREATE TABLE IF NOT EXISTS users (
+            user_id integer PRIMARY KEY, 
+            password text NOT NULL,
+            first_name text NOT NULL,
+            last_name text NOT NULL )
+        
+            ''')
+        # commit this action
+        self.conn.commit()
+        
+        
+        self.c.execute('''CREATE TABLE IF NOT EXISTS orders (
+            order_id integer PRIMARY KEY,
+            user_id integer NOT NULL,
+            table_number integer,
+            sales_subtotal real,
+            time_opened text,
+            time_closed text,
+            split integer,
+            guest_count,
+            FOREIGN KEY(user_id) REFERENCES users(user_id))
+            ''')
+        
+        # commit this action
+        self.conn.commit()
+        
+        # creates a sales table to keep track of individual items solds and the orders they
+        # belong to.
+        self.c.execute('''CREATE TABLE IF NOT EXISTS sales(
+            sales_id integer PRIMARY KEY,
+            item_id integer,
+            order_id,
+            FOREIGN KEY(item_id) REFERENCES items(item_id),
+            FOREIGN KEY(order_id) REFERENCES orders(order_id))
+            ''')
+        self.conn.commit()
+        
+        # creates an items table for each menu item
+        self.c.execute('''CREATE TABLE IF NOT EXISTS items(
+            item_id integer PRIMARY KEY,
+            price real NOT NULL,
+            name text NOT NULL)
+            ''')
+        self.conn.commit()
+        
+    def __del__(self):    
+        self.conn.close()
+    def addNewUser(self,u):
+        if isinstance(u,user):
+            self.c.execute('''INSERT INTO users(
+                password,first_name,last_name
+                )
+                VALUES (?,?,?)
+                ''',(u.pin,u.first_name,u.last_name))
+            self.conn.commit()
+    def getUsers(self):
+        self.c.execute(''' SELECT * FROM users
+            ''')
+        return self.c.fetchall()
+    
+if __name__ == '__main__':
+    db = posDatabase()
+    u = user("jesse", "harper", 123456)
+    db.addNewUser(u)
+    print(db.getUsers())
+        
+    
 
 
 
---
+++ examples/sqlite3_example/dataFields.py
@@ -0,0 +1,46 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class user():
+    def __init__(self,first_name,last_name,pin):
+        self.first_name = first_name
+        self.last_name = last_name
+        
+        self.pin = pin
+        self.tableOrders = []
+    def add_tableOrder(self,listIndex):
+        newTableOrder = tableOrder(listIndex)
+        self.tableOrder.append(newTableOrder)
+        
+        
+class menuItem():
+    def __init__(self, name, cost, index = 0):
+        self.name = name
+        self.cost = cost
+        self.index = index
+    def __str__(self):
+        return '{0:=<20s}  {1:>5.2f}'.format(self.name,self.cost)
+class tableOrder():
+    def __init__(self, m_user):
+        self.menuItems = []
+        self.m_user = m_user
+        self.m_subTotal = 0
+    def subTotal(self):
+        return sum(m.cost for m in self.menuItems)
+        
+    def add_menuItem(self, menuItem):
+        self.menuItems.append(menuItem)
+        self.m_subTotal = self.subTotal()
+        
+        
+if __name__ == "__main__":
+    newTableOrder = tableOrder(0)
+    for i in range(0,10):
+        newItem = menuItem("taco",4.00,i)
+        newTableOrder.add_menuItem(newItem)
+    cost = newTableOrder.subTotal()
+    print(cost)
+        
+        
+        
+        
